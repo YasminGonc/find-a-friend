54d2982bd4a635f80beec4b88a152d4eb242cb92
YasminGonc <yasmin.goncalves97@hotmail.com>
2023-03-18 22:13:16 -0300

    Add: themes and icon package

--- src/styles/themes/defaultTheme.ts
+++ src/styles/themes/defaultTheme.ts
@@ -2,6 +2,7 @@ export const defaultTheme = {
   colors: {
     primary: '#E44449',
     secondary: '#F15156',
+    select: '#F75F64',
     yellow: '#F4D35E',
     blue: '#0D3B66',
     bg: '#FDECED',
@@ -22,5 +23,10 @@ export const defaultTheme = {
       medium: '1.6rem',
       small: '1.2rem',
     },
+    lineHeight: '120%',
+  },
+  borderRadius: '1.5rem',
+  spacings: {
+    medium: '2rem',
   },
 } as const
